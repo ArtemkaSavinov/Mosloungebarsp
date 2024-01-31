68f52427a09d2dae478cd75624a21ce12302f648
Artem Savinov <artem.savinov2077@gmail.com>
2024-01-31 19:21:05 +0300

    page for new club

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-let express = require(`express`)
-let app = express()
-let port = 3000
+let express = require(`express`);
+let app = express();
+let port = 3000;
 
-let hbs = require(`hbs`)
-app.set(`views`, `views`)
-app.set(`view engine`, `hbs`)
+let hbs = require(`hbs`);
+app.set(`views`, `views`);
+app.set(`view engine`, `hbs`);
 
-app.use(express.static(`public`))
+app.use(express.static(`public`));
 
 app.listen(port, function () {
-    console.log(`Сервер запущен http://localhost:${port} `)
-})
+    console.log(`Сервер запущен http://localhost:${port} `);
+});
 
 app.get(`/`, function (req, res) {
-    res.render(`index`)
-})
+    res.render(`index`);
+});
+
+app.get('/versal', function(req, res) {
+    res.render('versal')
+});
